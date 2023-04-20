59944e5e1c650741c761b3c546cecde607b42aad
Jiří Syrovátko <113086098+JirkS@users.noreply.github.com>
2023-04-20 09:05:25 +0200

    Add files via upload

---
+++ next.php
@@ -0,0 +1,67 @@
+<?php 
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                   
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $successNext = TRUE;
+    $jeTamEmail = FALSE;
+    $file_name = 'data'. '.json';
+
+    $current_data = file_get_contents("$file_name");
+    $array = json_decode($current_data, TRUE);
+
+    foreach ($array as $insecure_val) {
+        if($insecure_val['email'] == $email) {
+            $jeTamEmail = TRUE;
+            if($insecure_val['password'] == $password){
+                $successNext = FALSE;
+                echo 'prihlasen';
+                break;
+            }
+        }
+        
+    }
+    if($jeTamEmail && $successNext == TRUE){
+        echo "spatne heslo";
+    }
+    
+    
+    function get_data() {
+        $email = $_POST['email'];
+        $file_name='data'. '.json';
+   
+        if(file_exists("$file_name")) { 
+            $current_data = file_get_contents("$file_name");
+            $array_data = json_decode($current_data, true);
+                               
+            $extra = array(
+                'email' => $_POST['email'],
+                'password' => $_POST['password'],
+            );
+            $array_data[]=$extra;
+            return json_encode($array_data);
+        }
+        else {
+            $datae=array();
+            $datae[]=array(
+                'email' => $_POST['email'],
+                'password' => $_POST['password'],
+            );
+            return json_encode($datae);   
+        }
+    }
+      
+    if($successNext && $jeTamEmail == FALSE){
+        if(file_put_contents("$file_name", get_data())) {
+            echo 'success';
+        }                
+        else {
+            echo 'There is some error';                
+        }
+    }
+    
+}
+ 
+?>
